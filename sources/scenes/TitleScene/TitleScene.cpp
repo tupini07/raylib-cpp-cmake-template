c6996a2477e713671337e4c40d1b602f10acb01a
Andrea Tupini <tupini07@gmail.com>
2023-01-24 09:39:16 -0600

    properly scale mouse position on title screen

--- sources/scenes/TitleScene/TitleScene.cpp
+++ sources/scenes/TitleScene/TitleScene.cpp
@@ -20,8 +20,8 @@ void TitleScene::draw()
 	const Vector2 text_size = MeasureTextEx(GetFontDefault(), text.c_str(), 20, 1);
 	DrawText(text.c_str(), GameConstants::WorldWidth / 2 - text_size.x / 2, texture_y + texture.height + text_size.y + 10, 20, BLACK);
 
-	int mouseX = GetMouseX();
-	int mouseY = GetMouseY();
+	int mouseX = GetMouseX() / ScreenScale;
+	int mouseY = GetMouseY() / ScreenScale;
 
 	int rectSize = GameConstants::WorldWidth / 20;
 	DrawRectangle(mouseX - rectSize / 2, mouseY - rectSize / 2, rectSize, rectSize, DARKPURPLE);
@@ -29,7 +29,7 @@ void TitleScene::draw()
 	DrawLine(mouseX, 0, mouseX, GameConstants::WorldHeight, SKYBLUE);
 	DrawLine(0, mouseY, GameConstants::WorldWidth, mouseY, GREEN);
 
-	DrawText("press 'c' to play!", 10, 10, 50, BLACK);
+	DrawText("press 'c' to play!", 10, 10, 30, BLACK);
 }
 
 Scenes TitleScene::update(float dt)
