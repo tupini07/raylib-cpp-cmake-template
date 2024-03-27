71155a7e151ee9148786059afdc5673b7d204d55
Andrea Tupini <tupini07@gmail.com>
2024-03-27 17:22:06 -0600

    clean up title scene

--- sources/scenes/TitleScene/TitleScene.cpp
+++ sources/scenes/TitleScene/TitleScene.cpp
@@ -8,29 +8,16 @@
 
 using namespace std;
 
-void TitleScene::draw()
+TitleScene::TitleScene()
 {
-	ClearBackground(RAYWHITE);
-
-	const int texture_x = GameConstants::WorldWidth / 2 - texture.width / 2;
-	const int texture_y = GameConstants::WorldHeight / 2.5 - texture.height / 2;
-	DrawTexture(texture, texture_x, texture_y, WHITE);
-
-	const string text = "I would like some potatoes please!";
-	const Vector2 text_size = MeasureTextEx(GetFontDefault(), text.c_str(), 20, 1);
-	DrawText(text.c_str(), GameConstants::WorldWidth / 2 - text_size.x / 2, texture_y + texture.height + text_size.y + 10, 20, BLACK);
-
-	int mouseX = GetMouseX() / ScreenScale;
-	int mouseY = GetMouseY() / ScreenScale;
-
-	int rectSize = GameConstants::WorldWidth / 20;
-	DrawRectangle(mouseX - rectSize / 2, mouseY - rectSize / 2, rectSize, rectSize, DARKPURPLE);
-
-	DrawLine(mouseX, 0, mouseX, GameConstants::WorldHeight, SKYBLUE);
-	DrawLine(0, mouseY, GameConstants::WorldWidth, mouseY, GREEN);
-
-	DrawText("press 'c' to play!", 10, 10, 30, BLACK);
+	texture = LoadTexture(AppConstants::GetAssetPath("test.png").c_str());
 }
+
+TitleScene::~TitleScene()
+{
+	UnloadTexture(texture);
+}
+
 
 Scenes TitleScene::update(float dt)
 {
@@ -42,12 +29,33 @@ Scenes TitleScene::update(float dt)
 	return Scenes::NONE;
 }
 
-TitleScene::TitleScene()
+void TitleScene::draw()
 {
-	texture = LoadTexture(AppConstants::GetAssetPath("test.png").c_str());
+	ClearBackground(RAYWHITE);
+
+	auto draw_with_backdrop = [](const string& text, int x, int y, int fontSize, Color color, Color backdropColor) {
+		DrawText(text.c_str(), x + 1, y + 1, fontSize, backdropColor);
+		DrawText(text.c_str(), x, y, fontSize, color);
+	};
+
+	draw_with_backdrop("Press 'c' to play!", 10, 10, 30, GOLD, BLACK);
+
+
+	const int texture_x = GameConstants::WorldWidth / 2 - texture.width / 2;
+	const int texture_y = GameConstants::WorldHeight / 2 - texture.height / 2;
+	DrawTexture(texture, texture_x, texture_y, WHITE);
+
+	const string text = "This is the Title Scene";
+	const int text_width = MeasureText(text.c_str(), 20);
+	DrawText(text.c_str(), GameConstants::WorldWidth / 2 - text_width / 2, texture_y + texture.height + 20, 20, BLACK);
+
+	int mouseX = GetMouseX() / ScreenScale;
+	int mouseY = GetMouseY() / ScreenScale;
+
+	int rectSize = GameConstants::WorldWidth / 20;
+	DrawRectangle(mouseX - rectSize / 2, mouseY - rectSize / 2, rectSize, rectSize, DARKPURPLE);
+
+	DrawLine(mouseX, 0, mouseX, GameConstants::WorldHeight, SKYBLUE);
+	DrawLine(0, mouseY, GameConstants::WorldWidth, mouseY, GREEN);
 }
 
-TitleScene::~TitleScene()
-{
-	UnloadTexture(texture);
-}
